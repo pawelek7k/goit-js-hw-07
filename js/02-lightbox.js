0ce0a45a2f129f805c23b146be60029417f338a2
pawelekk <144838381+pawelek7k@users.noreply.github.com>
2024-01-26 12:29:10 +0100

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,29 +2,21 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 const galleryList = document.querySelector(".gallery");
 
-for (let item of galleryItems) {
-  const galleryItem = `<li class="gallery__item">
-      <a class="gallery__link" href="${item.original}">
-      <img class="gallery__image" src="${item.preview}" alt="${item.description}" title="${item.description}"
-      o="${item.captionDelay}" data-source="${item.original}"/>
-      </a>
-      </li>`;
+const galleryItem = galleryItems.map((item) => `<li class="gallery__item">
+<a class="gallery__link" href="${item.original}">
+<img class="gallery__image" src="${item.preview}" alt="${item.description}" title="${item.description}"
+o="${item.captionDelay}" data-source="${item.original}"/>
+</a>
+</li>`).join("");
 
-  galleryList.innerHTML += galleryItem;
-}
+galleryList.innerHTML = galleryItem;
 
-var images = document.querySelectorAll(".gallery img");
-images.forEach(image => {
-  image.addEventListener("click", (event) => {
-    event.preventDefault();
-    const lightbox = new SimpleLightbox(".gallery a", {
+    var lightbox = new SimpleLightbox(".gallery a", {
       /* options */
     });
-  });
-});
 
 
 // // Change code below this line
 
-// console.log(galleryItems);
+console.log(galleryItems);
 
