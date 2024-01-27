cf818effdc47fe3467622ce2255a8375f29546ce
pawelekk <144838381+pawelek7k@users.noreply.github.com>
2024-01-27 17:29:10 +0100

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -4,15 +4,15 @@ const galleryList = document.querySelector(".gallery");
 
 const galleryItem = galleryItems.map((item) => `<li class="gallery__item">
 <a class="gallery__link" href="${item.original}">
-<img class="gallery__image" src="${item.preview}" alt="${item.description}" title="${item.description}"
-o="${item.captionDelay}" data-source="${item.original}"/>
+<img class="gallery__image" src="${item.preview}" alt="${item.description}" data-source="${item.original}"/>
 </a>
 </li>`).join("");
 
 galleryList.innerHTML = galleryItem;
 
     var lightbox = new SimpleLightbox(".gallery a", {
-      /* options */
+      captionDelay: 250,
+      captionsData: "alt",
     });
 
 
